439db0fa1b425974ce8f84995edaa03c71d49309
Your Name <abbasovelshen@mail.ru>
2023-02-27 23:13:24 +0400

    dfv

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+
+
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  let [list, setList] = useState([])
+  let [data, setData] = useState("avengers")
+  useEffect(() => {
+    fetch(` https://www.omdbapi.com/?s=${data}&apikey=278924d5`)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response.Search
+          )
+        setList(response.Search)});
+  }, [data])
+  console.log(list);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className='form'>
+        <label htmlFor='search'>search by film name : </label><br />
+        <input onChange={(e) => setData(e.target.value)} id='search' type="text" />
+
+      </form>
+      <div className='flex'>
+
+        {list?.map((item, index) => <div className='border' key={index}>
+          <img alt='dsfsd' src={item.Poster} />
+          <h3>{item.Title}</h3>
+          <span>Production Year {item.Year}</span>
+        </div>)
+        }
+
+
+      </div>
     </div>
   );
 }
